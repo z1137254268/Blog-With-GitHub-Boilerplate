5bf4a050197a55ba9a9d53a0b8b782f40cc921e1
z1137254268 <51695259+z1137254268@users.noreply.github.com>
2020-06-07 17:45:14 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,14 +19,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "Neu的博客"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
+author = "Neu"
+email = "1137254268@qq.com"
+author_homepage = "http://banszd.top"
 description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+key_words = ['Maverick', 'Neu', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
     {
